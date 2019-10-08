f2d83a84e0055cb6142c84ae0efe00f43e471b7e
Mackenzie Smith <smack11231@gmail.com>
2019-10-08 09:14:52 -0600

    Function 2 complete

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,3 +13,26 @@ puts three_even([2,1,4,6])
 puts three_even([1,4,6,4])
 puts three_even([])
 
+
+def bigger_two(list1, list2)
+    sum1 = 0
+    sum2 = 0
+    list1.each do |i|
+        sum1 = sum1 + i
+        i = i + 1
+    end
+    list2.each do |i|
+        sum2 = sum2 + i
+        i = i + 1
+    end
+    if sum1 > sum2
+        print list1
+    elsif sum2 > sum1
+        print list2
+    elsif sum1 == sum2
+        print list1
+    end
+end
+
+puts bigger_two([1,2],[3,4]) # [3, 4]
+puts bigger_two([1,7],[4,4]) # [1,7]
